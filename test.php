dde91a8fe662312c6452e2cfe3836fa86532a327
SNOWPC\hakoz <hakozaki.box@icloud.com>
2026-02-13 22:09:50 +0900

    テスト

---
+++ test.php
@@ -0,0 +1,23 @@
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    $箱崎（数字） = 20;
+    $箱崎（文字） = "22";
+
+    $箱崎（足し算結果）= $箱崎（数字）+ $箱崎（数字）;
+    $箱崎（足し算結果）= $箱崎（足し算結果）+ $箱崎（足し算結果）;
+
+    // $箱崎（足し算結果）変数の値が80だった場合、以下の処理を行う
+    if ($箱崎（足し算結果）= 10) {
+        $箱崎（足し算結果）= 100;
+    } elseif(){
+        
+    }
+
+    $箱崎（足し算結果）= $箱崎（足し算結果）* $箱崎（足し算結果）;
+}
